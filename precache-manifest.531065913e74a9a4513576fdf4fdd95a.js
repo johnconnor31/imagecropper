18654e36f9b444d6587be77b76d0361d496f13cc
Sairam S <sairam.singireesu@reltio.com>
2023-05-30 13:47:11 +0530

    Updates

---
+++ precache-manifest.531065913e74a9a4513576fdf4fdd95a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b4022bd081ae179ce8cff2b5e7bad7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3463d06f5e1470fa76ec",
+    "url": "/static/css/2.3cdd1eef.chunk.css"
+  },
+  {
+    "revision": "ef68a23c58383a0c19cc",
+    "url": "/static/css/main.d1dfdd6c.chunk.css"
+  },
+  {
+    "revision": "3463d06f5e1470fa76ec",
+    "url": "/static/js/2.9925fc09.chunk.js"
+  },
+  {
+    "revision": "ef68a23c58383a0c19cc",
+    "url": "/static/js/main.8822053a.chunk.js"
+  },
+  {
+    "revision": "b6c03980669069f02852",
+    "url": "/static/js/runtime-main.62302209.js"
+  }
+]);
